7f78349fd7f1464721ffe034e0b65dcc6c67df07
Victor Elizalde <vic-bull@hotmail.com>
2017-01-04 13:59:08 -0600

    Setup commit

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by Victor Elizalde on 1/4/17.
+//  Copyright © 2017 Victor Elizalde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        print("Camera pressed")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
